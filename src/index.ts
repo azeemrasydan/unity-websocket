1f4415cd7cf9bd026835296da2f78ae08a9d5f05
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-11-13 22:10:52 +0800

    Added kafka services to websocket unity

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 import WebSocket from 'ws';
+import { Kafka, Producer, ProducerRecord } from 'kafkajs';
+
+const kafka = new Kafka({
+  clientId: 'ball-event-producer',
+  brokers: ['127.0.0.1:9092'],
+});
+
+const producer = kafka.producer();
+
+const ballSelectedEvent = {
+  eventType: 'ballSelected',
+  ballId: '1',
+  timestamp: new Date().toISOString(),
+};
+
+const topic = 'ball_event_topic';
+
+const produce = async () => {
+  await producer.connect();
+
+  const message: ProducerRecord = {
+    topic,
+    messages: [
+      {
+        value: JSON.stringify(ballSelectedEvent),
+      },
+    ],
+  };
+
+  await producer.send(message);
+
+  await producer.disconnect();
+};
+
+produce().catch((err) => {
+  console.error('Error producing to Kafka:', err);
+  process.exit(1);
+});
 
 const wss = new WebSocket.Server({ port: 8080 });
 
@@ -6,7 +44,6 @@ wss.on('connection', (ws: WebSocket) => {
   console.log('New client connected');
 
   ws.on('message', (message: string) => {
-    console.log(`Received message: ${message}`);
     ws.send(`Server received your message: ${message}`);
   });
 
